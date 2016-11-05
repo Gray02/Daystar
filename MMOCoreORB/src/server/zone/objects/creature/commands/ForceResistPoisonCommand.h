89446c94d7df5106c024d016296e7d5c1068b91d
Skyyyr <skydeath227@yahoo.com>
2016-11-05 06:52:28 -0700

    Update ForceResistPoisonCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceResistPoisonCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceResistPoisonCommand.h
@@ -11,19 +11,59 @@ class ForceResistPoisonCommand : public JediQueueCommand {
 public:
 
 	ForceResistPoisonCommand(const String& name, ZoneProcessServer* server)
-		: JediQueueCommand(name, server) {
+		: QueueCommand(name, server) {
 
-		buffCRC = BuffCRC::JEDI_RESIST_POISON;
-
-		skillMods.put("resistance_poison", 25);
-		skillMods.put("absorption_poison", 25);
-
-	}
+}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-		return doJediSelfBuffCommand(creature);
-	}
+		
+		if (!checkStateMask(creature))
+			return INVALIDSTATE;
 
+		if (!checkInvalidLocomotions(creature))
+			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		uint32 forceResistPoisonCRC = BuffCRC::JEDI_RESIST_POISON;
+
+		
+		if(creature->hasBuff(forceResistPoisonCRC)) {
+			creature->sendSystemMessage("@jedi_spam:force_buff_present"); //"You already have a similar Force enhancement active."
+			return GENERALERROR;
+		}
+		
+		// Force cost of skill.
+		int forceCost = 100;
+                
+		ManagedReference<PlayerObject*> playerObject = creature->getPlayerObject();
+		int resist = (creature->getSkillMod("force_resist"));
+		int resistBle = (creature->getSkillMod("resistance_poison"));
+		int absorbBle = (creature->getSkillMod("absorption_poison"));
+
+		if (playerObject->getForcePower() <= forceCost) {
+			creature->sendSystemMessage("You don't have enough Force to preform this ability");
+			return false;
+		}
+                              
+                //Need updated strings
+		StringIdChatParameter startStringId("jedi_spam", "apply_forceresistpoison");
+		StringIdChatParameter endStringId("jedi_spam", "remove_forceresistpoison");
+
+		int duration = 360 + resist;
+		ManagedReference<Buff*> buff = new Buff(creature, forceResistPoisonCRC, duration, BuffType::JEDI);
+		buff->setStartMessage(startStringId);
+		buff->setEndMessage(endStringId);
+		buff->setSkillModifier("resistance_poison", 25 + resist + resistBle);
+		buff->setSkillModifier("absorption_poison", 25 + resist + absorbBle);
+		creature->addBuff(buff);
+		
+	        playerObject->setForcePower(playerObject->getForcePower() - forceCost);
+		creature->playEffect("clienteffect/pl_force_resist_poison_self.cef", "");
+                return SUCCESS;
+        }
 };
 
 #endif //FORCERESISTPOISONCOMMAND_H_
