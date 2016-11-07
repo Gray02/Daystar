8dec2423c4463495e80e8fa75088113c8e44eca5
Skyyyr <skydeath227@yahoo.com>
2016-11-07 01:49:52 -0700

    Update HealActionSelf1Command.h
    attempt #1

--- MMOCoreORB/src/server/zone/objects/creature/commands/HealActionSelf1Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/HealActionSelf1Command.h
@@ -1,7 +1,3 @@
-/*
-				Copyright <SWGEmu>
-		See file COPYING for copying conditions. */
-
 #ifndef HEALACTIONSELF1COMMAND_H_
 #define HEALACTIONSELF1COMMAND_H_
 
@@ -10,7 +6,79 @@
 
 class HealActionSelf1Command : public ForceHealQueueCommand {
 public:
-	HealActionSelf1Command(const String& name, ZoneProcessServer* server) : ForceHealQueueCommand(name, server) {}
+
+	HealActionSelf1Command(const String& name, ZoneProcessServer* server)
+: ForceHealQueueCommand(name, server) {
+
+	}
+
+	bool canPerformSkill(CreatureObject* creature) {
+		if (!creature->hasDamage(CreatureAttribute::ACTION)) {
+			creature->sendSystemMessage("@jedi_spam:no_damage_heal_self"); // You have no damage of that type.
+			return false;
+		}
+
+		return true;
+	}
+
+	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
+
+		if (!checkStateMask(creature))
+			return INVALIDSTATE;
+
+		if (!checkInvalidLocomotions(creature))
+			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		if (isWarcried(creature)) {
+			return GENERALERROR;
+		}
+
+
+		if (!checkForceCost(creature)) {
+			creature->sendSystemMessage("@jedi_spam:no_force_power");
+			return GENERALERROR;
+		}
+
+		// At this point, the player has enough Force... Can they perform skill?
+
+		if (!canPerformSkill(creature))
+			return GENERALERROR;
+
+
+		int forceCostDeducted = forceCost;
+
+		// Lets see how much healing they are doing.
+		int healAmount = 500;
+
+		uint32 actionHealed = creature->healDamage(creature, CreatureAttribute::ACTION, healAmount);
+
+
+		forceCostDeducted = MIN((actionHealed / 7), forceCost);
+
+
+		// Send system message(s).
+
+		if (actionHealed > 0){
+			StringIdChatParameter message2("jedi_spam", "heal_self");
+			message2.setDI(actionHealed);
+			message2.setTO("@jedi_spam:action_damage");
+			creature->sendSystemMessage(message2);
+		}
+
+		doAnimations(creature, creature);
+
+
+		ManagedReference<PlayerObject*> playerObject = creature->getPlayerObject();
+
+		if (playerObject != NULL)
+			playerObject->setForcePower(playerObject->getForcePower() - forceCostDeducted);
+
+		return SUCCESS;
+	}
 };
 
 #endif //HEALACTIONSELF1COMMAND_H_
