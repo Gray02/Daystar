d96adf62b5d8577b1c21921e51884b14af98cf08
Skyyyr <skydeath227@yahoo.com>
2016-11-08 10:13:59 -0700

    Update RevokeSkillCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
@@ -18,6 +18,8 @@ public:
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
 	}
+	
+	return SUCCESS;
 };
 
 #endif //REVOKESKILLCOMMAND_H_
