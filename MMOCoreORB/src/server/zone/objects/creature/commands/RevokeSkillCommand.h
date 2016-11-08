dc1d9a3b381bd752ccef2acf2d2193423c73f386
Skyyyr <skydeath227@yahoo.com>
2016-11-08 09:55:29 -0700

    Update RevokeSkillCommand.h
    attempt #1

--- MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
@@ -16,13 +16,44 @@ public:
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-
 		if (!checkStateMask(creature))
 			return INVALIDSTATE;
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+
+		if (object == NULL || !object->isCreatureObject())
+			return INVALIDTARGET;
+
+		CreatureObject* targetCreature = cast<CreatureObject*>( object.get());
+
+		Locker clocker(targetCreature, creature);
+
+		SkillManager* skillManager = SkillManager::instance();
+		skillManager->surrenderSkill(arguments.toString(), targetCreature, true);
+		bool skillGranted = skillManager->removeSkill(arguments.toString(), targetCreature, true, true, true);
+
+		if (skillGranted) {
+			StringIdChatParameter params;
+			params.setTO(arguments.toString());
+			params.setStringId("skill_teacher", "skill_terminal_grant");
+
+			targetCreature->sendSystemMessage(params);
+
+			creature->sendSystemMessage("Removed skill " + arguments.toString()
+					+ "from " + targetCreature->getFirstName());
+		} else {
+			StringIdChatParameter params;
+			params.setTO(arguments.toString());
+			params.setStringId("skill_teacher", "prose_train_failed");
+
+			targetCreature->sendSystemMessage(params);
+
+			creature->sendSystemMessage("Failed to remove skill " + arguments.toString()
+					+ "from " + targetCreature->getFirstName());
+		}
 		return SUCCESS;
 	}
 
