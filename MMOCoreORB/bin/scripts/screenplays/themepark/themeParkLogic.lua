swgemu/Core3

[moved] Faction status from PlayerObject to TangibleObject [fixed] Player names to properly flash when going overt to covert and covert to overt
Change-Id: I1ffee06db371656e89bf423eb13827d2e329d3fd

MMOCoreORB/bin/scripts/screenplays/themepark/themeParkLogic.lua
@@ -265,14 +265,8 @@ function ThemeParkLogic:isOnLeave(pPlayer)
 	if (pPlayer == nil) then
 		return false
 	end
-
-	local pGhost = CreatureObject(pPlayer):getPlayerObject()
-
-	if (pGhost == nil) then
-		return false
-	end
-
-	return PlayerObject(pGhost):isOnLeave()
+	
+	return CreatureObject(pPlayer):isOnLeave()
 end
 
 function ThemeParkLogic:hasEnoughFaction(pPlayer)
@@ -2036,12 +2030,12 @@ function ThemeParkLogic:followPlayer(pConversingNpc, pConversingPlayer)
 	AiAgent(pConversingNpc):setFollowObject(pConversingPlayer)
 
 	local playerFaction = CreatureObject(pConversingPlayer)
-	if (playerFaction == FACTIONREBEL or playerFaction == FACTIONIMPERIAL) and not PlayerObject(pGhost):isOnLeave() then
+	if (playerFaction == FACTIONREBEL or playerFaction == FACTIONIMPERIAL) and not CreatureObject(pConversingNpc):isOnLeave() then
 		CreatureObject(pConversingNpc):setFaction(playerFaction)
 
-		if PlayerObject(pGhost):isOvert() then
+		if CreatureObject(pConversingNpc):isOvert() then
 			CreatureObject(pConversingNpc):setPvpStatusBitmask(5)
-		elseif PlayerObject(pGhost):isCovert() then
+		elseif CreatureObject(pPlayer):isCovert() then
 			CreatureObject(pConversingNpc):setPvpStatusBitmask(1)
 		end
 	end

MMOCoreORB/src/server/zone/objects/creature/commands/SquadLeaderCommand.h
@@ -84,33 +84,23 @@ public:
 		if (leaderGhost == NULL)
 			return false;
 
-		PlayerObject* targetGhost = NULL;
+		CreatureObject* targetCreo = target;
 
-		if (allowPet && target->isPet()) {
-			ManagedReference<CreatureObject*> owner = target->getLinkedCreature().get();
-
-			if (owner != NULL && owner->isPlayerCreature()) {
-				targetGhost = owner->getPlayerObject();
-			}
-		} else {
-			targetGhost = target->getPlayerObject();
-		}
-
-		if (targetGhost == NULL)
-			return false;
+		if (allowPet && target->isPet())
+			targetCreo = target->getLinkedCreature().get();
 
 		if (leader->getFaction() != 0 && target->getFaction() != 0) {
-			if (leader->getFaction() != target->getFaction() && targetGhost->getFactionStatus() != FactionStatus::ONLEAVE)
+			if (leader->getFaction() != target->getFaction() && targetCreo->getFactionStatus() != FactionStatus::ONLEAVE)
 				return false;
 
-			if (leader->getFaction() == target->getFaction() && leaderGhost->getFactionStatus() == FactionStatus::COVERT && targetGhost->getFactionStatus() == FactionStatus::OVERT)
+			if (leader->getFaction() == target->getFaction() && leader->getFactionStatus() == FactionStatus::COVERT && targetCreo->getFactionStatus() == FactionStatus::OVERT)
 				return false;
 
-			if (leaderGhost->getFactionStatus() == FactionStatus::ONLEAVE && targetGhost->getFactionStatus() != FactionStatus::ONLEAVE)
+			if (leader->getFactionStatus() == FactionStatus::ONLEAVE && leader->getFactionStatus() != FactionStatus::ONLEAVE)
 				return false;
 		}
 
-		if (leader->getFaction() == 0 && target->getFaction() != 0 && targetGhost->getFactionStatus() != FactionStatus::ONLEAVE)
+		if (leader->getFaction() == 0 && target->getFaction() != 0 && targetCreo->getFactionStatus() != FactionStatus::ONLEAVE)
 			return false;
 
 		return true;

Gray02/Daystar

MMOCoreORB/bin/scripts/screenplays/themepark/themeParkLogic.lua
@@ -265,14 +265,8 @@ function ThemeParkLogic:isOnLeave(pPlayer)
 	if (pPlayer == nil) then
 		return false
 	end
-
-	local pGhost = CreatureObject(pPlayer):getPlayerObject()
-
-	if (pGhost == nil) then
-		return false
-	end
-
-	return PlayerObject(pGhost):isOnLeave()
+	
+	return CreatureObject(pPlayer):isOnLeave()
 end
 
 function ThemeParkLogic:hasEnoughFaction(pPlayer)
@@ -2036,12 +2030,12 @@ function ThemeParkLogic:followPlayer(pConversingNpc, pConversingPlayer)
 	AiAgent(pConversingNpc):setFollowObject(pConversingPlayer)
 
 	local playerFaction = CreatureObject(pConversingPlayer)
-	if (playerFaction == FACTIONREBEL or playerFaction == FACTIONIMPERIAL) and not PlayerObject(pGhost):isOnLeave() then
+	if (playerFaction == FACTIONREBEL or playerFaction == FACTIONIMPERIAL) and not CreatureObject(pConversingPlayer):isOnLeave() then
 		CreatureObject(pConversingNpc):setFaction(playerFaction)
 
-		if PlayerObject(pGhost):isOvert() then
+		if CreatureObject(pConversingPlayer):isOvert() then
 			CreatureObject(pConversingNpc):setPvpStatusBitmask(5)
-		elseif PlayerObject(pGhost):isCovert() then
+		elseif CreatureObject(pConversingPlayer):isCovert() then
 			CreatureObject(pConversingNpc):setPvpStatusBitmask(1)
 		end
 	end

MMOCoreORB/src/server/zone/objects/creature/commands/SquadLeaderCommand.h
@@ -84,33 +84,23 @@ public:
 		if (leaderGhost == NULL)
 			return false;
 
-		PlayerObject* targetGhost = NULL;
+		CreatureObject* targetCreo = target;
 
-		if (allowPet && target->isPet()) {
-			ManagedReference<CreatureObject*> owner = target->getLinkedCreature().get();
-
-			if (owner != NULL && owner->isPlayerCreature()) {
-				targetGhost = owner->getPlayerObject();
-			}
-		} else {
-			targetGhost = target->getPlayerObject();
-		}
-
-		if (targetGhost == NULL)
-			return false;
+		if (allowPet && target->isPet())
+			targetCreo = target->getLinkedCreature().get();
 
 		if (leader->getFaction() != 0 && target->getFaction() != 0) {
-			if (leader->getFaction() != target->getFaction() && targetGhost->getFactionStatus() != FactionStatus::ONLEAVE)
+			if (leader->getFaction() != target->getFaction() && targetCreo->getFactionStatus() != FactionStatus::ONLEAVE)
 				return false;
 
-			if (leader->getFaction() == target->getFaction() && leaderGhost->getFactionStatus() == FactionStatus::COVERT && targetGhost->getFactionStatus() == FactionStatus::OVERT)
+			if (leader->getFaction() == target->getFaction() && leader->getFactionStatus() == FactionStatus::COVERT && targetCreo->getFactionStatus() == FactionStatus::OVERT)
 				return false;
 
-			if (leaderGhost->getFactionStatus() == FactionStatus::ONLEAVE && targetGhost->getFactionStatus() != FactionStatus::ONLEAVE)
+			if (leader->getFactionStatus() == FactionStatus::ONLEAVE && targetCreo->getFactionStatus() != FactionStatus::ONLEAVE)
 				return false;
 		}
 
-		if (leader->getFaction() == 0 && target->getFaction() != 0 && targetGhost->getFactionStatus() != FactionStatus::ONLEAVE)
+		if (leader->getFaction() == 0 && target->getFaction() != 0 && targetCreo->getFactionStatus() != FactionStatus::ONLEAVE)
 			return false;
 
 		return true;