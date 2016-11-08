7326582b0ea61e420bf2522c2a9fadc6bf8e5bb5
Skyyyr <skydeath227@yahoo.com>
2016-11-08 10:06:43 -0700

    Update RevokeSkillCommand.h
    reverted

--- MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
@@ -6,6 +6,7 @@
 #define REVOKESKILLCOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/managers/skill/SkillManager.h"
 
 class RevokeSkillCommand : public QueueCommand {
 public:
@@ -16,47 +17,7 @@ public:
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-		if (!checkStateMask(creature))
-			return INVALIDSTATE;
-
-		if (!checkInvalidLocomotions(creature))
-			return INVALIDLOCOMOTION;
-
-		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
-
-		if (object == NULL || !object->isCreatureObject())
-			return INVALIDTARGET;
-
-		CreatureObject* targetCreature = cast<CreatureObject*>( object.get());
-
-		Locker clocker(targetCreature, creature);
-
-		SkillManager* skillManager = SkillManager::instance();
-		skillManager->surrenderSkill(arguments.toString(), targetCreature, true);
-		bool skillGranted = skillManager->surrenderSkill(arguments.toString(), targetCreature, true);
-
-		if (targetCreature->hasSkill(skillGranted)) {
-			StringIdChatParameter params;
-			params.setTO(arguments.toString());
-			params.setStringId("skill_teacher", "skill_terminal_grant");
-
-			targetCreature->sendSystemMessage(params);
-
-			creature->sendSystemMessage("Removed skill " + arguments.toString()
-					+ "from " + targetCreature->getFirstName());
-		} else {
-			StringIdChatParameter params;
-			params.setTO(arguments.toString());
-			params.setStringId("skill_teacher", "prose_train_failed");
-
-			targetCreature->sendSystemMessage(params);
-
-			creature->sendSystemMessage("Failed to remove skill " + arguments.toString()
-					+ "from " + targetCreature->getFirstName());
-		}
-		return SUCCESS;
 	}
-
 };
 
 #endif //REVOKESKILLCOMMAND_H_
