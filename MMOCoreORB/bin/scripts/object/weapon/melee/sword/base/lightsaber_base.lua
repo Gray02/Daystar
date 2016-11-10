48e6e9c947e21b5e545782d76a60f04a742fe2ce
Skyyyr <skydeath227@yahoo.com>
2016-11-10 03:32:10 -0700

    Update lightsaber_base.lua
    test

--- MMOCoreORB/bin/scripts/object/weapon/melee/sword/base/lightsaber_base.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/sword/base/lightsaber_base.lua
@@ -43,6 +43,7 @@
 
 object_weapon_melee_sword_base_lightsaber_base = object_weapon_melee_sword_base_shared_lightsaber_base:new {
 	--weaponType = ONEHANDJEDIWEAPON,
+	objectMenuComponent = "LightsaberObjectMenuComponent",
 	weaponType = ONEHANDMELEEWEAPON,
 
 }
