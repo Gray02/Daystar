437f3e1b75b80ed162d79f4ae82acba1725dec54
TOXIC <thrax989@yahoo.com>
2016-11-11 02:27:36 -0800

    Updated Ct

--- MMOCoreORB/bin/scripts/staff/levels/ct.lua
+++ MMOCoreORB/bin/scripts/staff/levels/ct.lua
@@ -4,18 +4,7 @@ ct = {
 	tag = "CT",
 	skills = {
 		"admin_base",
-		"admin_debug_01",
-		"admin_general_admin_01",
-		"admin_general_admin_02",
-		"admin_general_admin_03",
-		"admin_player_management_01",
-		"admin_player_management_02",
-		"admin_player_management_03",
-		"admin_quest_management_01",
-		"admin_quest_management_02",
-		"admin_server_admin_01",
-		"admin_spawn_management_01",
-		"admin_spawn_management_02"
+		"admin_general_admin_01"
 	}
 }
 
