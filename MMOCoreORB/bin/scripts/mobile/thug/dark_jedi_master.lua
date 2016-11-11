2f1dbb2fb183268806c653c8e5416f071df61da6
kurdtkobain <dbz81487@yahoo.com>
2016-11-11 06:39:31 -0500

    BLUE!!!!!!!!

--- MMOCoreORB/bin/scripts/mobile/thug/dark_jedi_master.lua
+++ MMOCoreORB/bin/scripts/mobile/thug/dark_jedi_master.lua
@@ -49,7 +49,7 @@ dark_jedi_master = Creature:new {
 	weapons = {"dark_jedi_weapons_gen4"},
 	conversationTemplate = "",
 	attacks = merge(lightsabermaster,forcepowermaster),
-	lsColor=3
+	lsColor=4
 }
 
 CreatureTemplates:addCreatureTemplate(dark_jedi_master, "dark_jedi_master")
